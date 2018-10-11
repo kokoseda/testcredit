0b7393e44b4206775628490b8f22696443089698
kokoseda <kokoseda22@yahoo.com>
2018-10-12 00:10:38 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+var ServerID = "495608433064673281"; //اي دي السيرفر
+var ChannelID = "495608433064673283";// اي دي الروم
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('warn', console.warn);
+
+client.on('error', console.error);
+
+
+
+client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script'));
+
+client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
+
+client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));
+
+
+function timerFunc() {
+    client.on('message', msg => {
+        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7))
+
+
+    });
+}
+
+var timer = setTimeout(timerFunc, 1000);
+
+client.login(process.env.BOT_TOKEN);
